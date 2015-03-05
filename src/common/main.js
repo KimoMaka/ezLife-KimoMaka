e269238d9de05198dd39671cf9072df7dc1bac3f
Lyntor Paul G. Figueroa <theotakupage@gmail.com>
2015-03-05 17:38:53 +0800

    Play time with kango
    Just playing around with the Kango Framework before we start

--- src/common/main.js
+++ src/common/main.js
@@ -1,15 +1,29 @@
-﻿function MyExtension() {
+﻿/*!
+ * ezLife AutoIn - The most easiest Anime i.ntere.st bot ever made
+ * https://github.com/KazeFlame/ezLife-AutoIn/
+ *
+ * Copyright 2015 by KazeFlame and contributors
+ * Released under the MIT license
+ * https://github.com/KazeFlame/ezLife-AutoIn/blob/master/LICENSE
+ *
+ */
+
+function ezLifeAutoIn() {
     var self = this;
-    kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function() {
-        self._onCommand();
+    
+    kango.ui.browserButton.setPopup({
+        url: 'ui/home.html',
+        width: 200,
+        height: 300
     });
+    
+    kango.ui.browserButton.addEventListener(kango.ui.browserButton.event.COMMAND, function(){
+        self._onCommand();
+    })
 }
 
-MyExtension.prototype = {
-
-    _onCommand: function() {
-        kango.browser.tabs.create({url: 'http://kangoextensions.com/'});
-    }
+ezLifeAutoIn.prototype = {
+    _onCommand: function(){}
 };
 
-var extension = new MyExtension();
+var extension = new ezLifeAutoIn();
